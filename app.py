e1df89498ea99ba85448d59ef7e7407f9fe167bb
zhang zihang <129568431+goodday1024@users.noreply.github.com>
2024-11-04 22:40:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,23 +16,29 @@ class MyClient(botpy.Client):
 
     async def on_at_message_create(self, message: Message):
         _log.info(message.author.avatar)
-        if "请求音乐：" in message.content:
+        if "/请求音乐" in message.content:
+            _log.info(message.author.avatar)
+            name = message.content.split("-")[0]
+            page = message.content.split("-")[1]
+            _log.info(f"name:{name},page:{page}")
             params={
-                'input':message.split("-")[1],
+                'input':name,
                 'filter':'name',
                 'type':"netease",
-                'page':int(message.split("-")[2]),
+                'page':int(page),
             }
             headers={
                 'X-Requested-With': 'XMLHttpRequest',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',}
             url='https://www.at38.cn/'
             response=requests.post(url,data=params,headers=headers)
-            datas=response.json()
+            datas=json.loads(response.text)
             songs = {}
-            for data in datas['data']:
-                songs[data[0]['title']] = data[0]['url']
-            _log.info(message.author.username)
+            print(datas)
+            title = datas.data[0].title
+            url = datas.data[0].url
+            songs[str(title)] = str(url)
+        _log.info(message.author.username)
         await message.reply(content=f"已经帮您查询到相关歌曲：{songs}")
 
 if __name__ == '__main__':
